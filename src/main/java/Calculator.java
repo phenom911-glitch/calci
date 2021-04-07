198a6ec08bfadf01751e299a2d9367a3573363ab
imphenom <nouman.nazir1993@gmail.com>
2021-04-07 15:29:38 +0500

    subtract function and testcase added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,7 @@ public class Calculator {
     public int add(int a, int b) {
         return a+b;
     }
+    public int sub(int a, int b){
+        return 0;
+    }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -5,10 +5,18 @@ public class TestCalculator {
     Calculator cal= new Calculator();
 
     @Test
-    public void addFuncTestSimple() {
+    public void testAdd() {
 
         int result=cal.add(2, 2);
         if(result!=4) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testSub(){
+        int result = cal.sub(3,2);
+        if(result != 1){
             Assert.fail();
         }
     }
